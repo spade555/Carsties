030960d0f7c24f614eed7292b50fc3ae363f8186
Gergely Szabo <spade555@gmail.com>
2025-10-27 22:03:26 +0100

    End of Section 02

---
+++ src/AuctionService/Mappers/RequestMapper.cs
@@ -0,0 +1,20 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.Mappers;
+
+public class RequestMapper : Profile
+{
+    public RequestMapper()
+    {
+        // entity => DTO
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+
+        // DTO => entity
+        CreateMap<CreateAuctionDto, Auction>()
+            .ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
